1df8749fe2a95beb9ffbb3cd415d880416abd0a4
John Scott (Azure) <joscot@microsoft.com>
2023-02-23 16:01:14 -0800

    first commit

---
+++ CloneAllGHOrgRepos.ps1
@@ -0,0 +1,17 @@
+param(
+    [Parameter(Mandatory=$true)]
+    $orgName
+)
+$repos = ConvertFrom-Json (gh repo list $orgName -L 20 --json name)
+foreach ($repo in $repos) {
+    $repoName = $repo.name
+    if (Test-Path $repoName) {
+        Write-Host "Skipping $repoName"
+        continue
+    }
+    Write-Host "Cloning $repoName"
+    gh repo clone $orgName/$repoName
+}
+
+
+
