2c6d82df5b1443ded13615e36d51e7e142b25e32
John Scott (Azure) <joscot@microsoft.com>
2023-11-16 13:10:23 -0800

    Update pull

--- PullAllDirs.ps1
+++ PullAllDirs.ps1
@@ -3,21 +3,29 @@
 #this is a good way to update all your git repos at once
 #   
 param(
-    [Parameter(Mandatory=$true)]
-    $dirPath ="./",
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
+    $dirPath = "./",
+    [Parameter(Mandatory = $true)]
     $orgName
 )
-   $localRepos = Get-ChildItem $dirPath -Directory
-   $ghRepos = ConvertFrom-Json (gh repo list $orgName -L 100 --json name)
-   foreach ($repo in $localRepos) {
-         if ($ghRepos.name -notcontains $repo.Name) {
-              Write-Host "$repo does not exist in $orgName"
-              #Remove-Item $repo.FullName -recurse -force
-              Continue
-         }
-       Write-Host "Pulling $repo"
-       Push-Location $repo
-       git pull
-       Pop-Location
-   }
+Push-Location $dirPath
+#$localRepos = Get-ChildItem $dirPath -Directory
+$ghRepos = ConvertFrom-Json (gh repo list $orgName -L 100 --json name)
+foreach ($repo in $ghRepos) {
+    #  if ($ghRepos.name -notcontains $repo.Name) {
+    #       Write-Host "$repo does not exist in $orgName"
+    #Remove-Item $repo.FullName -recurse -force
+    #       Continue
+    #  }
+    $repoName = $repo.name
+    if (!(Test-Path $repoName)) {
+        Write-Host "Cloning $repoName"
+        gh repo clone $orgName/$repoName
+    }
+    else {
+        Write-Host "Pulling $repoName"
+        Push-Location $repo.Name
+        git pull
+        Pop-Location
+    }
+}
